50c4a47145e084ccda314d5764a81b6e9a1a95cf
Samantha Cruz <cruz.s98319@gmail.com>
2024-02-10 13:36:00 -0800

    First push. Added forEach function and changeTitle function  in admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,37 @@
+async function main() {
+    let books = await fetch('http://localhost:3001/listBooks')
+    let booksJSON = await books.json()
+    
+    booksJSON.forEach(function(book) {
+        let div = document.createElement('div')
+        div.innerHTML = `
+            <img src = "${book.imageURL}"[width="200"] />
+            <h3>${book.title}</h3>
+            <p>Year published: ${book.year}</p>
+            <p>Quantity: ${book.quantity}</p>
+            <input id="${book.id}" type="text" />
+            <input type="submit" onclick="changeTitle(${book.id})" />
+        `
+        document.body.append(div)
+    })
+}
 
-// Your Code Here
+main()
+
+async function changeTitle(id) {
+    let input = document.getElementById(id)
+    let value = input.value
+
+    let response = await fetch('http://localhost:3001/updateBook', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: id,
+            title: value
+        })
+    })
+    let responseJSON = await response.json()
+    console.log(responseJSON)
+}
