a46eafe4a986b321d3d722aa91e86f08c91a3ccf
Samantha Cruz <cruz.s98319@gmail.com>
2024-02-11 22:00:34 -0800

    Fixed bug where the submit buttons were not connected to the correct input box. Also added two parameters to the functions

--- public/admin.js
+++ public/admin.js
@@ -1,27 +1,27 @@
 async function main() {
     let books = await fetch('http://localhost:3001/listBooks')
     let booksJSON = await books.json()
-    
+
     booksJSON.forEach(function(book) {
         let div = document.createElement('div')
         div.innerHTML = `
             <img src = "${book.imageURL}" width="200" />
             <h3 id="title-${book.id}">${book.title}</h3>
             <p>Year published: ${book.year}</p>
-            <p>Quantity: ${book.quantity}</p>
-            <input id="${book.id}" type="text" />
-            <input type="submit" onclick="changeTitle(${book.id})" />
-            <input id="${book.num}" type="text" />
-            <input type="submit" onclick="changeQuantity(${book.num})" />
+            <p id="quantity-${book.id}">Quantity: ${book.quantity}</p>
+            <input id="title-input-${book.id}" type="text" />
+            <input type="submit" onclick="changeTitle('title-input-${book.id}', ${book.id})" />
+            <input id="quantity-input-${book.id}" type="text" />
+            <input type="submit" onclick="changeQuantity('quantity-input-${book.id}', ${book.id})" />
         `
         document.body.append(div)
     })
 }
 
-main()
+main ()
 
-async function changeTitle(id) {
-    let input = document.getElementById(id)
+async function changeTitle(inputId, bookId) {
+    let input = document.getElementById(inputId)
     let value = input.value
 
     let response = await fetch('http://localhost:3001/updateBook', {
@@ -30,18 +30,18 @@ async function changeTitle(id) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            id: id,
+            id: bookId,
             title: value
         })
     })
     let responseJSON = await response.json()
     console.log(responseJSON)
 
-    document.getElementById(`title-${id}`).textContent = value
+    document.getElementById(`title-${bookId}`).textContent = value
 }
 
-async function changeQuantity(num) {
-    let input = document.getElementById(num)
+async function changeQuantity(inputId, bookId) {
+    let input = document.getElementById(inputId)
     let value = input.value
 
     let response = await fetch('http://localhost:3001/updateBook', {
@@ -50,12 +50,12 @@ async function changeQuantity(num) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            id: num,
+            id: bookId,
             quantity: value
         })
     })
     let responseJSON = await response.json()
     console.log(responseJSON)
 
-    document.getElementById(`quantity-${num}`).textContent = value
+    document.getElementById(`quantity-${bookId}`).textContent = "Quantity: " + value
 }
