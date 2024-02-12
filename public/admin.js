18ee9be0a87d44bb78feccf509dc064fff59a609
Samantha Cruz <cruz.s98319@gmail.com>
2024-02-11 19:51:11 -0800

    Attempted to add an input to change the quantity of books by duplicating the async function changeTitle. TypeError occurs when testing on the local host.

--- public/admin.js
+++ public/admin.js
@@ -11,6 +11,8 @@ async function main() {
             <p>Quantity: ${book.quantity}</p>
             <input id="${book.id}" type="text" />
             <input type="submit" onclick="changeTitle(${book.id})" />
+            <input id="${book.num}" type="text" />
+            <input type="submit" onclick="changeQuantity(${book.num})" />
         `
         document.body.append(div)
     })
@@ -36,4 +38,24 @@ async function changeTitle(id) {
     console.log(responseJSON)
 
     document.getElementById(`title-${id}`).textContent = value
+}
+
+async function changeQuantity(num) {
+    let input = document.getElementById(num)
+    let value = input.value
+
+    let response = await fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: num,
+            quantity: value
+        })
+    })
+    let responseJSON = await response.json()
+    console.log(responseJSON)
+
+    document.getElementById(`quantity-${num}`).textContent = value
 }
